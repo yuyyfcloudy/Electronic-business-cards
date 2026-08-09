99c248d0d93f7a1184ce27a88fce67348d32b65c
yuyyfcloudy <2237194962@qq.com>
2026-08-09 11:18:35 +0800

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,19 +1,6 @@
-import { createClient }
-from
-'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.108.0/+esm'
 
+const supabaseUrl = "https://qzxzsnxrjppfjnxxmxkt.supabase.co"
+const supabaseKey = "sb_publishable_Ik24Q_jg-mxVutTdXqjbbw_Kn0xohJu"
 
-const supabaseUrl =
-"https://qzxzsnxrjppfjnxxmxkt.supabase.co"
-
-
-const supabaseKey =
-"sb_publishable_Ik24Q_jg-mxVutTdXqjbbw_Kn0xohJu"
-
-
-
-export const supabase =
-createClient(
-    supabaseUrl,
-    supabaseKey
-)
+export const supabase = createClient(supabaseUrl, supabaseKey)
